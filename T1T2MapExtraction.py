386f21794d3e91a60eba79bd91d860f95f683f7e
Hari Rallapalli <hari.rallapalli@gmail.com>
2021-08-22 22:41:14 -0400

    Adding some useful scripts from grad school to be refined.

---
+++ T1T2MapExtraction.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 18:24:03 2021
+
+@author: Hari Rallapalli
+"""
+
+
+import os
+import numpy as np
+import nibabel as nib
+
+input_path = r'G:\MINCVM\PCLKO\PCP2-DTR\maps\\'
+output_path = r'G:\MINCVM\PCLKO\PCP2-DTR\maps\Extracted\\'
+
+for file in os.listdir(input_path):
+    if file.endswith('.nii.gz'):
+        
+        
+        image_name = file
+        img = nib.load((input_path + image_name))
+        
+        img_data = img.get_fdata()
+        img_data = np.asarray(img_data)
+        sizer = np.asarray(img_data.shape)
+        
+        sub_img = np.empty((sizer[0],sizer[1]))
+        affine = np.diag([1,2,3,4])
+        
+        sub_img = img_data[:,:,2]
+        
+        sub_img_nii = nib.Nifti1Image(sub_img,affine)
+        
+        nib.save(sub_img_nii,(output_path+image_name[:-7]+'_MapExtracted.nii.gz'))
